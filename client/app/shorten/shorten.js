5ca002e49500aea777aeecb4f46081b093aa374d
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 16:04:02 -0700

    finish and move the completed version of shorten.js

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,11 +1,18 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  $scope.link = {}; 
+  // Your code here
 
-  $scope.addLink = function() {
-    return Links.addOne();
+  $scope.link = {};
+  $scope.addLink = function () {
+    $scope.loading = true;
+    Links.addOne($scope.link)
+      .then(function () {
+        $scope.loading = false;
+        $location.path('/');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
-
-
-});
+  });
