7cb34bf4c05acf82a96cd98e0b79da675b6815c9
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 16:02:41 -0700

    refactor of the services.js

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,29 +1,31 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  var getAll = function() {
+  // Your code here
+
+  var getAll = function () {
     return $http({
       method: 'GET',
-      url: '/api/links'  
-    }).then(function (resp) {  
+      url: '/api/links'
+    })
+    .then(function (resp) {
       return resp.data;
     });
   };
-  var addOne = function(url) {
+
+  var addOne = function (link) {
     return $http({
-      method: 'POST', 
-      url: '/api/links', 
-      data: url
+      method: 'POST',
+      url: '/api/links',
+      data: link
     });
   };
 
   return {
     getAll: getAll,
     addOne: addOne
-  }; 
-
-})
-
+  };
+  })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
